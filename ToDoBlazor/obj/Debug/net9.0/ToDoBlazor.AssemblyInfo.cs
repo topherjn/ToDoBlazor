f1ce7efcf3d2e3ecd0304039ec26184b55a3e328
topherjn <nicholsonc@gmail.com>
2025-05-14 19:51:17 -0400

    Adding a todo item works.

--- ToDoBlazor/obj/Debug/net9.0/ToDoBlazor.AssemblyInfo.cs
+++ ToDoBlazor/obj/Debug/net9.0/ToDoBlazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoBlazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eee2214ddfd1d1bb1c125624ac8023505ace578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f63ac83f9ce20f07bf36f1865670930e69903923")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoBlazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoBlazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
